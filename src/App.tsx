2662af79f29c455b026822176a6bae5f5b027779
Sony AK <sony@sony-ak.com>
2025-10-10 09:34:29 +0700

    feat: add LineShadowText component and integrate it into App; update components.json and package files for motion support

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { LineShadowText } from "@/components/ui/line-shadow-text";
+
 export default function App() {
   return (
     <>
-      <h1>Hello, respate!</h1>
+      <div className="flex h-screen w-full items-center justify-center">
+        <h1 className="text-balance text-5xl font-semibold leading-none tracking-tighter sm:text-6xl md:text-7xl lg:text-8xl">
+          <LineShadowText className="italic" shadowColor={"black"}>
+            respate
+          </LineShadowText>
+        </h1>
+      </div>
     </>
   );
 }
